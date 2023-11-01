42aa37c114e896f57a46ac8081bc1767f05eedb1
Philippe Wanuty <philippewanuty@hotmail.com>
2023-11-01 18:16:19 -0300

    Starting project Rocket Notes

---
+++ src/Pages/Details/Details.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+export function Details() {
+	return (
+		<Container>
+			<div ><h1>Hello World!</h1>
+      <p className="nome1">Philippe Wanuty</p></div>
+		</Container>
+	);
+}
