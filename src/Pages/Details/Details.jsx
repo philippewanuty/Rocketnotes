d09bb298ff60bf8120a9ef56dc29914b406b76e1
Philippe Wanuty <philippewanuty@hotmail.com>
2023-11-16 20:30:25 -0300

    Aplicando o Tag e o Button Text  na Page Details

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,26 +1,34 @@
 import { Button } from '../../components/Button';
+import { ButtonText } from '../../components/ButtonText';
 import { Header } from '../../components/Header';
 import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
 
 import { Container, Links } from './styles';
 
 export function Details() {
 	return (
-		<Container>
-			<Header title='Philippe Wanuty' />
-			<Section title='Links Úteis'>
-				<Links>
-					<li>
-						<a href='#'>https://www.rocketseat.com.br/</a>
-					</li>
-					<li>
-						<a href='#'>https://www.rocketseat.com.br/</a>
-					</li>
-				</Links>
-			</Section>
+    <Container>
+      <Header title="Philippe Wanuty" />
+      <ButtonText title="Excluir a nota"/>
 
-			<Section title='Marcadores' />
-			<Button title='Entrar' />
-		</Container>
-	);
+      <Section title="Links Úteis">
+        <Links>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
+      </Section>
+
+      <Button title="Entrar" />
+    </Container>
+  );
 }
