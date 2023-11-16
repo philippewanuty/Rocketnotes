004bd32d8f95e7e38d36613c0bf18d32c89851b6
Philippe Wanuty <philippewanuty@hotmail.com>
2023-11-16 06:28:00 -0300

    Adicionando header e o section no details

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,10 +1,26 @@
-import { Container } from "./styles";
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
+
+import { Container, Links } from './styles';
 
 export function Details() {
 	return (
 		<Container>
-			<div ><h1>Hello World!</h1>
-      <p className="nome1">Philippe Wanuty</p></div>
+			<Header title='Philippe Wanuty' />
+			<Section title='Links Úteis'>
+				<Links>
+					<li>
+						<a href='#'>https://www.rocketseat.com.br/</a>
+					</li>
+					<li>
+						<a href='#'>https://www.rocketseat.com.br/</a>
+					</li>
+				</Links>
+			</Section>
+
+			<Section title='Marcadores' />
+			<Button title='Entrar' />
 		</Container>
 	);
 }
