d58647e7af4bc1b747f04b011b5cc4616484803f
Philippe Wanuty <philippewanuty@hotmail.com>
2023-11-18 21:08:42 -0300

    testando a page  home

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Details } from './Pages/Details/Details'
+// import { Details } from './Pages/Details/Details'
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from './styles/global';
-
-
+import { Home } from './Pages/Home';
 
 import theme from "./styles/theme"
+
 
 
 
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
 			<GlobalStyles />
-			<Details />
+			<Home />
 		</ThemeProvider>
 	</React.StrictMode>
 );
