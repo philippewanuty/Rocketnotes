a595ff08334322ab6e75bb3aeb3e4bebc063d323
Philippe Wanuty <philippewanuty@hotmail.com>
2023-11-18 21:09:26 -0300

    Finalizando pagina Details

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -4,31 +4,40 @@ import { Header } from '../../components/Header';
 import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
 
-import { Container, Links } from './styles';
+import { Container, Links, Content } from './styles';
 
 export function Details() {
 	return (
-    <Container>
-      <Header title="Philippe Wanuty" />
-      <ButtonText title="Excluir a nota"/>
+		<Container>
+			<Header title='Philippe Wanuty' />
 
-      <Section title="Links Úteis">
-        <Links>
-          <li>
-            <a href="#">https://www.rocketseat.com.br/</a>
-          </li>
-          <li>
-            <a href="#">https://www.rocketseat.com.br/</a>
-          </li>
-        </Links>
-      </Section>
-
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="nodejs" />
-      </Section>
-
-      <Button title="Entrar" />
-    </Container>
-  );
+			<main>
+				<Content>
+					<ButtonText title='Excluir a nota' />
+					<h1>Introdução ao React</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur fugiat optio vero, veritatis, quod saepe aliquam officia consequuntur pariatur, nemo delectus dolor. Optio, placeat sequi
+						magnam nostrum minima rem ad. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa corrupti, sit, aperiam eos mollitia impedit blanditiis velit perferendis unde est commodi dolorem
+						nesciunt officiis inventore minus at eum incidunt ut?
+					</p>
+		
+					<Section title='Links úteis'>
+						<Links>
+							<li>
+								<a href='#'>https://www.rocketseat.com.br/</a>
+							</li>
+							<li>
+								<a href='#'>https://www.rocketseat.com.br/</a>
+							</li>
+						</Links>
+					</Section>
+					<Section title='Marcadores'>
+						<Tag title='express' />
+						<Tag title='nodejs' />
+					</Section>
+					<Button title='Entrar' />
+				</Content>
+			</main>
+		</Container>
+	);
 }
--- src/Pages/Details/styles.js
+++ src/Pages/Details/styles.js
@@ -1,28 +1,63 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-
 	width: 100%;
 	height: 100vh;
 
 	display: grid;
-
 	grid-template-rows: 105px auto;
-
 	grid-template-areas:
 		'header'
 		'content';
+
+	> main {
+		
+		grid-area: content;
+		overflow-y: scroll;
+		padding: 64px 0;
+		
+	
+	}
 `;
 
 export const Links = styled.ul`
 
-list-style: none;
+	list-style: none;
 
->li {
+	> li {
+		margin-top: 12px;
 
-	margin-top: 12px;
-}
-	a {
+		a {
+			color: ${({ theme }) => theme.COLORS.WHITE};
+		}
+	}
+`;
+
+export const Content = styled.div`
+
+	max-width: 550px;
+	margin: 0 auto;
+
+	display: flex;
+	flex-direction: column;
+
+	> h1 {
+		font-size: 2.225rem;
+		font-weight: 500;
+		padding-top: 4rem;
 		color: ${({ theme }) => theme.COLORS.WHITE};
+	}
+
+	p {
+		padding-top: 1rem;
+		padding-bottom: 1.625rem;
+		font-size: 1rem;
+		font-weight: 400;
+		text-align: justify;
+	}
+
+	Button:nth-last-child(1){
+		margin-top: 129px;
+	
 	}
 `;
