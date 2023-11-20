b9af0fcf951e3b695a04b57ac681e192928094e1
Philippe Wanuty <philippewanuty@hotmail.com>
2023-11-19 21:30:10 -0300

    Componente input

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from './style';
+import { RiAddFill } from 'react-icons/ri';
+
+
+export function Input({ placeholder}) {
+  return (
+			<Container type='text' id='textInput' placeholder={placeholder}>
+		
+			</Container>
+		);
+};
