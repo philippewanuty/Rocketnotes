256591e565b5e9275864e14aeea4b0286bf62d34
Philippe Wanuty <philippewanuty@hotmail.com>
2023-11-21 09:28:20 -0300

    Trocando a renderização no main
    Trocando a renderização no main para estilizar a pagina SignIn

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 // import { Details } from './Pages/Details/Details'
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from './styles/global';
-import { Home } from './Pages/Home';
+import { SignIn } from './Pages/SignIn';
 
 import theme from "./styles/theme"
 
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
 			<GlobalStyles />
-			<Home />
+			<SignIn />
 		</ThemeProvider>
 	</React.StrictMode>
 );
